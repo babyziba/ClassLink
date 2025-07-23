f64a7c7015b6e591f4ab69546ae788b75570b21f
jjforsyth15 <jjforsyth15@gmail.com>
2025-07-22 20:53:33 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import MatchList from './components/MatchList';
-import mockProfiles from './data/mockProfiles';
-import './App.css';
-import Home from './pages/get_started.js';
+// import React from 'react';
+// import MatchList from './components/MatchList';
+// import mockProfiles from './data/mockProfiles';
+// import './App.css';
+// import Home from './pages/get_started.js';
+
+// function App() {
+//   return (
+//     <div className="App" style={{padding: '2rem', fontFamily: 'sans-serif'}}>
+//       <h1>Class link header </h1>
+//         <p>
+//         Find classmates with shared classes and interests.
+//         <MatchList profiles={mockProfiles} />
+//         <button style={{ padding: '0.6rem 1.2rem', fontSize: '1rem' }}>Get Started</button>
+//         </p>
+//     </div>
+//   );
+// }
+
+
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import get_started from "./pages/get_started.js";
 
 function App() {
   return (
-    <div className="App" style={{padding: '2rem', fontFamily: 'sans-serif'}}>
-      <h1>Class link header </h1>
-        <p>
-        Find classmates with shared classes and interests.
-        <MatchList profiles={mockProfiles} />
-        <button style={{ padding: '0.6rem 1.2rem', fontSize: '1rem' }}>Get Started</button>
-        </p>
-    </div>
-  );
+    <Router>
+      <Route path="/" element={<Navigate to="/get_started" />} />
+      <Route path="/getstarted" element={<Navigate to="/get_started" />} />
+    </Router>
+  )
 }
 
 export default App;
+
+
+
