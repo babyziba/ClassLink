34e078128b4e9953302866a80aae2052252ebd86
jjforsyth15 <jjforsyth15@gmail.com>
2025-08-07 16:33:09 -0700

    Merge before-main

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-=======
-// import React from 'react';
-// import MatchList from './components/MatchList';
-// import mockProfiles from './data/mockProfiles';
-// import './App.css';
-// import Home from './pages/get_started.js';
-
-// function App() {
-//   return (
-//     <div className="App" style={{padding: '2rem', fontFamily: 'sans-serif'}}>
-//       <h1>Class link header </h1>
-//         <p>
-//         Find classmates with shared classes and interests.
-//         <MatchList profiles={mockProfiles} />
-//         <button style={{ padding: '0.6rem 1.2rem', fontSize: '1rem' }}>Get Started</button>
-//         </p>
-//     </div>
-//   );
-// }
-
-
->>>>>>> origin/beforemain
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import GetStarted from "./pages/get_started.js";
 import Signup from "./pages/signup.js";
@@ -41,11 +18,4 @@ function App() {
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
-
-
-
->>>>>>> origin/beforemain
