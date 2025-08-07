ca95e7a536b35ade863586d818aebd246af82b32
jjforsyth15 <jjforsyth15@gmail.com>
2025-08-07 16:49:19 -0700

    megre get_started

--- src/pages/get_started.js
+++ src/pages/get_started.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './get_started.css';
 import students from '../assets/students.png'; // image import
-<<<<<<< HEAD
-
-function GetStarted() {
-=======
 import { useNavigate, Link } from 'react-router-dom';
 
 function GetStarted() {
 
   const navigate = useNavigate();
 
->>>>>>> origin/beforemain
   return (
     <div className="home-container">
       <header className="home-header">
@@ -27,11 +22,7 @@ function GetStarted() {
       <main className="home-main">
         <div className="text-section">
           <h1>Find classmates with<br />shared interests<br />and classes</h1>
-<<<<<<< HEAD
-          <button className="get-started-btn">get started</button>
-=======
           <button className="get-started-btn" onClick={() => navigate("/signup")}>get started</button>
->>>>>>> origin/beforemain
         </div>
         <div className="image-section">
           <img src={students} alt="Students talking" />
